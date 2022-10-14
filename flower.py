bd2cdae34c5a58774cbf1b3060cb79770cd30f9d
Bhuvnesh Brawar <90771976+BhuvneshB@users.noreply.github.com>
2022-10-14 19:27:41 +0530

    Update flower.py

--- flower.py
+++ flower.py
@@ -2,8 +2,11 @@ import turtle
 i=0
 while i<100:
     #turtle.position()
+    turtle.pensize(i/10)
     turtle.forward(i)
+    turtle.pencolor(i/100,i*i/10000,i*i*i/10**6)
     turtle.right(45)
     turtle.forward(i)
     turtle.circle(i,270)
     i+=1
+
